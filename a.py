f52a7d450d7acd8e45fb5af39f79a59caed8554f
anuarsux <113997971+anuarsux@users.noreply.github.com>
2022-09-20 16:05:26 +0200

    Update a.py

--- a.py
+++ a.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("MY FIRST ML APP")
 st.markdown("#Main Page")
 st.sidebar.markdown("MainPage")
-my_text = st.text("A ramdom version 2 of text")
+my_text = st.text("Janko, Bruno and Nils are:")
 my_button = st.button("puchale aquí")
 if my_button:
-	st.title("The model is running")
+	st.title("My friends!")
